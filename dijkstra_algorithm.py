383e4020622cdb285d8e65d9b05380a7771195d7
abako220 <abako220@gmail.com>
2020-03-16 14:36:00 +0100

    fix compilation error in codebase

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -13,9 +13,9 @@ class Graph():
 					for row in range(vertices)] 
 
 	def printSolution(self, dist): 
-		print "Vertex \tDistance from Source"
+		print ("Vertex \tDistance from Source")
 		for node in range(self.V): 
-			print node, "\t", dist[node] 
+			print (node, "\t", dist[node])
 
 	# A utility function to find the vertex with 
 	# minimum distance value, from the set of vertices 
@@ -23,7 +23,7 @@ class Graph():
 	def minDistance(self, dist, sptSet): 
 
 		# Initilaize minimum distance for next node 
-		min = sys.maxint 
+		min = sys.maxsize 
 
 		# Search not nearest vertex not in the 
 		# shortest path tree 
@@ -39,7 +39,7 @@ class Graph():
 	# using adjacency matrix representation 
 	def dijkstra(self, src): 
 
-		dist = [sys.maxint] * self.V 
+		dist = [sys.maxsize] * self.V 
 		dist[src] = 0
 		sptSet = [False] * self.V 
 
@@ -59,8 +59,7 @@ class Graph():
 			# distance is greater than new distance and 
 			# the vertex in not in the shotest path tree 
 			for v in range(self.V): 
-				if self.graph[u][v] > 0 and sptSet[v] == False and \ 
-				dist[v] > dist[u] + self.graph[u][v]: 
+				if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]: 
 						dist[v] = dist[u] + self.graph[u][v] 
 
 		self.printSolution(dist) 
